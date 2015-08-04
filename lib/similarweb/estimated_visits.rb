0120899715b8059247e9274c79c3d343780ff545
Prakhar Sharma <prakhar@akaruilabs.com>
2015-08-04 10:56:44 +0530

    Changed the end point from estimated traffic to visits

--- lib/similarweb/estimated_visits.rb
+++ lib/similarweb/estimated_visits.rb
@@ -1,7 +1,7 @@
 module Similarweb
   module EstimatedVisits
     def estimated_visits(domain)
-      response = self.http_client.get "#{domain}/v1/EstimatedTraffic?Format=JSON&UserKey=#{self.api_key}"
+      response = self.http_client.get "#{domain}/v1/visits?Format=JSON&UserKey=#{self.api_key}"
       JSON(response.body)
     end
   end
